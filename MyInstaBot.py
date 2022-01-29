49a99fd2787f04b08332d7480507a35e279a1a93
wroomie407 <Sebastian_Mou@protonmail.com>
2022-01-29 13:13:32 -0600

    error on accounts

--- MyInstaBot.py
+++ MyInstaBot.py
@@ -3,26 +3,37 @@ from logic import MyInstaBot_login, testerr
 import tester_file
 
 def main_menu():
-    print("1. selenium")
+    print("1. Instagram Marketing Bot")
     print("2. testerr")
     print("3. funcion")
+    print("4. whatsapp")
     print("0. exit")
+
     option = int(input("entter a number: "))
 
     if option == 1:
-        def main():
-            while True:
-                my_bot = MyInstaBot_login()
-                sleep(60*60)
+        print("this marketing boy will comment & like on chosen hashtags")
+        print("and will comment & like difrent insta accounts")
+        op = str(input("do you what to begin (y/n): "))
+        if op == "y":
+            def main():
+                while True:
+                    my_bot = MyInstaBot_login()
+                    sleep(60*60)
 
-        if __name__ == '__main__':
-            main()
+            if __name__ == '__main__':
+                main()
+        else:
+            print("ok bye")
 
     elif option == 2:
         testerr.tester_print()
 
     elif option == 3:
         tester_file.just_a_funcion()
+
+    elif option == 4:
+        print("4")
     
     elif option == 0:
         print("good bye")
--- logic.py
+++ logic.py
@@ -8,12 +8,21 @@ class MyInstaBot_login():
     links = []
 
     comments = [
-        'hermoso', 'wow'
+        'very beautiful', 'very gorgeous', 'looks grate'
     ]
 
+    #hashtag = ['luxlife', 'bugatti']
+
+    accounts = [
+        'charlidamelio', 'dixiedamelio,'
+    ]
+    #'🔥🔥🔥', 'beautiful 😍😍',
     def __init__(self):
-        self.login('','')
-        self.likes_comment_and_hashtag('')
+        username = str(input("enter username: "))
+        password = str(input("enter password: "))
+        self.login(username, password)
+        self.likes_comment_and_hashtag('luxlife')
+        self.comment_on_account()
 
     def login(self, username, password):
         PATH = "C:\Program Files (x86)\\chromedriver"
@@ -29,6 +38,27 @@ class MyInstaBot_login():
         self.driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/div/div/div/button').click()
         sleep(2)
         self.driver.find_element_by_xpath("//button[contains(text(), 'Not Now')]").click()
+
+    def comment_on_account(self):
+        for account in self.accounts:
+            #go to profile page
+            self.driver.get('https://www.instagram.com/{}/'.format(account))
+
+            # get most resent photo
+            links = self.driver.find_element_by_tag_name('a')
+            def condition(link):
+                return '.com/p/' in link.get_attribute('href')
+            valid_links = list(filter(condition, links))
+            last_photo_url = valid_links[0].get_attribute('href')
+            self.driver.get(last_photo_url)
+
+            # comment on photos
+            self.driver.find_element_by_class_name('RxpZH').click()
+            sleep(1)
+            comment_this_bitch = self.driver.find_element_by_xpath("//textarea[@aria-label='Add a comment…']")
+            comment_this_bitch.send_keys(self.comments[randint(0,2)])
+            sleep(1)
+            self.driver.find_element_by_xpath("//button[@type='submit']").click()
 
     def likes_comment_and_hashtag(self, hashtage):
         self.driver.get('https://www.instagram.com/explore/tags/{}/'.format(hashtage))
@@ -53,10 +83,12 @@ class MyInstaBot_login():
             # comments
             self.driver.find_element_by_class_name('RxpZH').click()
             sleep(1)
-            self.driver.find_element_by_xpath("//textarea[@placeholder='Add a comment_']").send_keys(self.comments[randint(0,1)])
+            comment_this_bitch = self.driver.find_element_by_xpath("//textarea[@aria-label='Add a comment…']")
+            comment_this_bitch.send_keys(self.comments[randint(0,2)])
             sleep(1)
             self.driver.find_element_by_xpath("//button[@type='submit']").click()
 
 class testerr():
     def tester_print():
         print("printed")
+
